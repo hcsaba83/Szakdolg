f016efc6d74e063f06ebfb9faf66e2b1165d4143
Horváth Csaba <hcsaba83@hotmail.com>
2018-11-25 20:17:29 +0100

    16. commit. Prepared for Tomcat

--- src/main/java/com/szakdolg/SzakDolgApplication.java
+++ src/main/java/com/szakdolg/SzakDolgApplication.java
@@ -2,11 +2,27 @@ package com.szakdolg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
+@EnableConfigurationProperties
+@PropertySources({
+    @PropertySource(value = "file:${appConf}", ignoreResourceNotFound = true)
+})
 @SpringBootApplication
-public class SzakDolgApplication {
+public class SzakDolgApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SzakDolgApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(SzakDolgApplication.class);
+    }
+
+	
 }
